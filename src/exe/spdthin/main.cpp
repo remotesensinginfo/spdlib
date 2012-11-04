f7e86b1ecc25a2c4476d59f92e8d160dd95ee525
Pete Bunting <6181851+petebunting@users.noreply.github.com>
2012-11-04 22:11:05 +1300

    Update to include a new command to thin a LiDAR dataset.

---
+++ src/exe/spdthin/main.cpp
@@ -0,0 +1,100 @@
+/*
+ *  main.cpp
+ *  spdlib
+ *
+ *  Created by Pete Bunting on 04/11/2012.
+ *  Copyright 2012 SPDLib. All rights reserved.
+ *
+ *  This file is part of SPDLib.
+ *
+ *  SPDLib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SPDLib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SPDLib.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+#include <spd/tclap/CmdLine.h>
+
+#include "spd/SPDTextFileUtilities.h"
+#include "spd/SPDException.h"
+#include "spd/SPDProcessPulses.h"
+#include "spd/SPDProcessingException.h"
+#include "spd/SPDThinPulses.h"
+
+#include "spd/spd-config.h"
+
+int main (int argc, char * const argv[])
+{
+    std::cout << "spdrmnoise " << SPDLIB_PACKAGE_STRING << ", Copyright (C) " << SPDLIB_COPYRIGHT_YEAR << " Sorted Pulse Library (SPD)\n";
+	std::cout << "This program comes with ABSOLUTELY NO WARRANTY. This is free software,\n";
+	std::cout << "and you are welcome to redistribute it under certain conditions; See\n";
+	std::cout << "website (http://www.spdlib.org). Bugs are to be reported on the trac\n";
+	std::cout << "or directly to " << SPDLIB_PACKAGE_BUGREPORT << std::endl;
+	
+	try
+	{
+        TCLAP::CmdLine cmd("Thin a point cloud to a defined bin spacing: spdthin", ' ', "1.0.0");
+        
+        TCLAP::ValueArg<boost::uint_fast32_t> numOfRowsBlockArg("r","blockrows","Number of rows within a block (Default 100)",false,100,"unsigned int");
+		cmd.add( numOfRowsBlockArg );
+        
+        TCLAP::ValueArg<boost::uint_fast32_t> numOfColsBlockArg("c","blockcols","Number of columns within a block (Default 0) - Note values greater than 1 result in a non-sequencial SPD file.",false,0,"unsigned int");
+		cmd.add( numOfColsBlockArg );
+        
+        TCLAP::ValueArg<float> binSizeArg("b","binsize","Bin size for processing and output image (Default 0) - Note 0 will use the native SPD file bin size.",false,0,"float");
+		cmd.add( binSizeArg );
+        
+        TCLAP::ValueArg<boost::uint_fast16_t> numPulsesArg("n","numpulses","Number of pulses within the bin (Default 1).",false,1,"unsigned int");
+		cmd.add( numPulsesArg );
+        
+		TCLAP::UnlabeledMultiArg<std::string> multiFileNames("Files", "File names for the input and output files", true, "string");
+		cmd.add( multiFileNames );
+		cmd.parse( argc, argv );
+		
+		std::vector<std::string> fileNames = multiFileNames.getValue();
+		if(fileNames.size() != 2)
+		{
+			for(unsigned int i = 0; i < fileNames.size(); ++i)
+			{
+				std::cout << i << ": " << fileNames.at(i) << std::endl;
+			}
+			
+            spdlib::SPDTextFileUtilities textUtils;
+			std::string message = std::string("Two file paths should have been specified (e.g., Input and Output). ") + textUtils.uInt32bittostring(fileNames.size()) + std::string(" were provided.");
+			throw spdlib::SPDException(message);
+		}
+        
+		std::string inputFile = fileNames.at(0);
+		std::string outputFile = fileNames.at(1);
+        
+        spdlib::SPDFile *spdInFile = new spdlib::SPDFile(inputFile);
+        spdlib::SPDPulseProcessor *pulseProcessor = new spdlib::SPDThinPulses(numPulsesArg.getValue());
+        spdlib::SPDSetupProcessPulses processPulses = spdlib::SPDSetupProcessPulses(numOfColsBlockArg.getValue(), numOfRowsBlockArg.getValue(), true);
+        processPulses.processPulsesWithOutputSPD(pulseProcessor, spdInFile, outputFile, binSizeArg.getValue());
+        delete pulseProcessor;
+        delete spdInFile;
+	}
+	catch (TCLAP::ArgException &e)
+	{
+		std::cerr << "Parse Error: " << e.what() << std::endl;
+	}
+	catch(spdlib::SPDException &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+	
+}
+
