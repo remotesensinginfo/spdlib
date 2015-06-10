e74650890af1f6e5fc5fe23f7133a2fcbb202913
Daniel Clewley <daniel.clewley@gmail.com>
2015-06-10 21:37:05 +0100

    LAS 1.3 waveform importer is now working. Only implemented for SPD import (not UPD). Needs more testing but waveforms look like they are in the correct location relative to points.

--- src/spd/SPDLASFileImporter.cpp
+++ src/spd/SPDLASFileImporter.cpp
@@ -250,32 +250,7 @@ namespace spdlib
                                 z1 = spdPt->z;                                
                                 
                                 spdPulse->pts->push_back(spdPt);
-                                
-                                /* If first return (only copy for one return) and file has waveform data
-                                 copy waveform to SPD Pulse
-                                 */
-                                if((i == 0) && haveWaveforms)
-                                {
-                                    if(laswaveformreader->read_waveform(&lasreader->point))
-                                    {
-                                        if(laswaveformreader->nsamples > 0)
-                                        {
-                                            
-                                            spdPulse->x0 = laswaveformreader->XYZreturn[0];
-                                            spdPulse->y0 = laswaveformreader->XYZreturn[1];
-                                            spdPulse->z0 = laswaveformreader->XYZreturn[2];
-
-                                            spdPulse->numOfReceivedBins = laswaveformreader->nsamples;
-                                            spdPulse->received = new boost::uint_fast32_t[spdPulse->numOfReceivedBins];
-                                            
-                                            for(unsigned int s = 0; s < spdPulse->numOfReceivedBins; s++)
-                                            {
-                                                spdPulse->received[s] = (unsigned int)laswaveformreader->samples[s];
-                                            }
-                                        }
-                                    }
-                                }
-                                
+                                                              
                                 ++numOfPoints;
                                 ++nPtIdx;
                             }
@@ -419,6 +394,18 @@ namespace spdlib
 	
 	std::vector<SPDPulse*>* SPDLASFileImporter::readAllDataToVector(std::string inputFile, SPDFile *spdFile)throw(SPDIOException)
 	{
+        /**
+        Class to read LAS format input data to a vector of SPDPulses.
+
+        Uses LASlib to read data.
+
+        For discrete return data (LAS 1.0 - 1.2) pulses are constructed using first and last return
+        and stored without a digitised waveform.
+
+        For full waveform data (LAS 1.3) the digitised waveform and additional pulse information is
+        stored for points where it is available.
+        
+        */
 		SPDPulseUtils pulseUtils;
 		std::vector<SPDPulse*> *pulses = new std::vector<SPDPulse*>();
 		boost::uint_fast64_t numOfPulses = 0;
@@ -432,9 +419,11 @@ namespace spdlib
 		double zMax = 0;
 		bool first = true;
 		bool firstZ = true;
+        bool lasHeaderInfoSet = false;
 		
 		classWarningGiven = false;
         bool haveWaveforms = false;
+        bool pulseHasWaveform = false;
 		
 		try 
 		{
@@ -494,7 +483,18 @@ namespace spdlib
 				std::cout << "Started (Read Data) ." << std::flush;
                 while (lasreader->read_point())
 				{
-					//std::cout << numOfPoints << std::endl;
+                    unsigned int lasindex = lasreader->point.wavepacket.getIndex();
+                    if(lasindex)
+                    {
+                        // Convert bin spacing to nano seconds
+                        if(!lasHeaderInfoSet)
+                        {
+                            spdFile->setTemporalBinSpacing(lasreader->header.vlr_wave_packet_descr[lasindex]->getTemporalSpacing()/1000);
+
+                            lasHeaderInfoSet = true;
+                        }
+                    }
+                    //std::cout << numOfPoints << std::endl;
 					if((reportedNumOfPts > 10) && ((numOfPoints % feedback) == 0))
 					{
 						std::cout << "." << feedbackCounter << "." << std::flush;
@@ -503,6 +503,8 @@ namespace spdlib
 					
                     if(lasreader->point.get_return_number() <= 1)
                     {
+                        pulseHasWaveform = false;
+
                         spdPt = this->createSPDPoint(lasreader->point);
                         ++numOfPoints;
                         
@@ -524,6 +526,9 @@ namespace spdlib
                             }
                         }
                         
+                        /* Set pulse origin based on first point
+                           If there is waveform data will use this
+                           instead */
                         x0 = spdPt->x;
                         y0 = spdPt->y;
                         z0 = spdPt->z;
@@ -540,7 +545,67 @@ namespace spdlib
                         {
                             spdPulse->pts->reserve(spdPulse->numberOfReturns);
                         }
-                        
+                                                
+                        /* If first return (only copy for one return) and file has waveform data
+                         copy waveform to SPD Pulse
+                         */
+                        if(haveWaveforms)
+                        {
+                            if(laswaveformreader->read_waveform(&lasreader->point))
+                            {
+                                if(laswaveformreader->nsamples > 0)
+                                {
+                                    pulseHasWaveform = true;
+
+                                    // Set number of samples
+                                    spdPulse->numOfReceivedBins = laswaveformreader->nsamples;
+
+                                    // Get pulse time
+                                    double pulse_duration = spdPulse->numOfReceivedBins*(lasreader->header.vlr_wave_packet_descr[lasindex]->getTemporalSpacing() / 1000);
+
+                                    /* Get the offset (in picoseconts) from the first digitized value
+                                    to the location within the waveform packet that the associated 
+                                    return pulse was detected.*/
+                                    double location = lasreader->point.wavepacket.getLocation();
+
+                                    // Save to point
+                                    spdPt->waveformOffset = location;
+
+                                    // Set pulse GPS time
+                                    spdPulse->gpsTime = spdPt->gpsTime - location*10E3;
+
+                                    /* Set the start location of the return pulse
+                                    This is calculated as the location of the first return 
+                                    minus the time offset multiplied by XYZ(t) */
+                                   
+                                    spdPulse->x0 = x0 - location*laswaveformreader->XYZt[0];
+                                    spdPulse->y0 = y0 - location*laswaveformreader->XYZt[1];
+                                    spdPulse->z0 = z0 - location*laswaveformreader->XYZt[2];
+
+                                    /* Get the end location of the return pulse
+                                    This is calculated as start location of the pulse
+                                    plus the pulse duration multipled by XYZ(t)
+                                    It is only used to get the azimuth and zenith angle 
+                                    of the pulse */
+                                    x1 = x0 + pulse_duration*laswaveformreader->XYZt[0];
+                                    y1 = y0 + pulse_duration*laswaveformreader->XYZt[1];
+                                    z1 = z0 + pulse_duration*laswaveformreader->XYZt[2];
+
+                                    // Set intensity gain and offset
+                                    spdPulse->receiveWaveGain = lasreader->header.vlr_wave_packet_descr[lasindex]->getDigitizerGain();
+                                    spdPulse->receiveWaveOffset = lasreader->header.vlr_wave_packet_descr[lasindex]->getDigitizerOffset();
+                                    
+                                    // Copy waveform data
+                                    spdPulse->received = new boost::uint_fast32_t[spdPulse->numOfReceivedBins];
+                                    for(unsigned int s = 0; s < spdPulse->numOfReceivedBins; s++)
+                                    {
+                                        spdPulse->received[s] = (unsigned int)laswaveformreader->samples[s];
+                                    }
+
+                                }
+                            }
+                        }
+         
                         if(lasreader->point.get_return_number() == 0)
                         {
                             nPtIdx = 1;
@@ -561,8 +626,6 @@ namespace spdlib
                                     std::cout << "." << feedbackCounter << "." << std::flush;
                                     feedbackCounter += 10;
                                 }
-                                
-                                //std::cout << "\tIn Pulse (Num Returns = " << pt.get_NumberOfReturns() << "): pt.get_ReturnNumber() = " << pt.get_ReturnNumber() << std::endl;
                                 
                                 if(nPtIdx != lasreader->point.get_return_number())
                                 {
@@ -587,38 +650,25 @@ namespace spdlib
                                     zMax = spdPt->z;
                                 }
                         
-                                x1 = spdPt->x;
-                                y1 = spdPt->y;
-                                z1 = spdPt->z;
-                                                        
-                                spdPulse->pts->push_back(spdPt);
-                                
-                                /* If first return (only copy for one return) and file has waveform data
-                                 copy waveform to SPD Pulse
-                                 */
-                                if((i == 0) && haveWaveforms)
+                                /* If waveform data is available x1, y1, z1
+                                will be the location of the last sample in the
+                                digitsed waveform.
+                                Otherwise will use last point */
+                                if(!pulseHasWaveform) 
                                 {
-                                    if(laswaveformreader->read_waveform(&lasreader->point))
-                                    {
-                                        if(laswaveformreader->nsamples > 0)
-                                        {
-                                            
-                                            spdPulse->x0 = laswaveformreader->XYZreturn[0];
-                                            spdPulse->y0 = laswaveformreader->XYZreturn[1];
-                                            spdPulse->z0 = laswaveformreader->XYZreturn[2];
-                                            
-                                            spdPulse->numOfReceivedBins = laswaveformreader->nsamples;
-                                            spdPulse->received = new boost::uint_fast32_t[spdPulse->numOfReceivedBins];
-                                            
-                                            for(unsigned int s = 0; s < spdPulse->numOfReceivedBins; s++)
-                                            {
-                                                spdPulse->received[s] = (unsigned int)laswaveformreader->samples[s];
-                                            }
-                                            
-                                        }
-                                    }
-                                }
+                                    x1 = spdPt->x;
+                                    y1 = spdPt->y;
+                                    z1 = spdPt->z;
+                                }                        
                                 
+                                /* If waveform data is available, set offset for
+                                each point to start of wavepacket */
+                                if(pulseHasWaveform)
+                                {
+                                    spdPt->waveformOffset = lasreader->point.wavepacket.getLocation();
+                                }
+                                spdPulse->pts->push_back(spdPt);
+                               
                                 ++numOfPoints;
                                 ++nPtIdx;
                             }
@@ -651,7 +701,7 @@ namespace spdlib
                             spdPulse->scanDirectionFlag = SPD_NEGATIVE;
                         }
                         
-                        if(spdPulse->numberOfReturns > 1)
+                        if((spdPulse->numberOfReturns > 1) | pulseHasWaveform)
                         {
                             range = std::sqrt(std::pow(x1-x0,2) + std::pow(y1-y0,2) + std::pow(z1-z0,2));
                             spdPulse->zenith = std::acos((z1-z0) / range);
@@ -675,10 +725,15 @@ namespace spdlib
                             spdPulse->xIdx = spdPulse->pts->front()->x;
                             spdPulse->yIdx = spdPulse->pts->front()->y;
                         }
-                        else if(indexCoords == SPD_LAST_RETURN)
+                        else if(indexCoords == SPD_START_OF_RECEIVED_WAVEFORM)
                         {
-                            spdPulse->xIdx = spdPulse->pts->back()->x;
-                            spdPulse->yIdx = spdPulse->pts->back()->y;
+                            spdPulse->xIdx = x0;
+                            spdPulse->yIdx = y0;
+                        }
+                        else if(indexCoords == SPD_END_OF_RECEIVED_WAVEFORM)
+                        {
+                            spdPulse->xIdx = x1;
+                            spdPulse->yIdx = y1;
                         }
                         else
                         {
@@ -733,17 +788,18 @@ namespace spdlib
 				}
 				spdFile->setNumberOfPulses(numOfPulses);
 				spdFile->setNumberOfPoints(numOfPoints);
-				spdFile->setOriginDefined(SPD_FALSE);
 				spdFile->setDiscretePtDefined(SPD_TRUE);
 				spdFile->setDecomposedPtDefined(SPD_FALSE);
 				spdFile->setTransWaveformDefined(SPD_FALSE);
                 if(haveWaveforms)
                 {
                     spdFile->setReceiveWaveformDefined(SPD_TRUE);
+				    spdFile->setOriginDefined(SPD_TRUE);
                 }
                 else
                 {
                     spdFile->setReceiveWaveformDefined(SPD_FALSE);
+				    spdFile->setOriginDefined(SPD_FALSE);
                 }
 			}
 			else 
